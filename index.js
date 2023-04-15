0c6f02ee45f90bcc15413208e3e77df15e815006
mouad <mouad.bouthaich@outlook.com>
2023-04-15 16:01:23 +0000

    ~Initial Commit
    Working on an authentication issue

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const sharepoint = require("sharepointplus/dist");
+const { JSDOM } = require('jsdom');
+
+// Create a fake window object
+const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
+global.window = dom.window;
+global.document = dom.window.document;
+
+console.log(process.env.SHAREPOINT_USERNAME);
+
+// define the SharePoint credentials
+const credentials = {
+  username: process.env.SHAREPOINT_USERNAME,
+  password: process.env.SHAREPOINT_PASSWORD,
+};
+
+// create a new SharePointPlus instance with the credentials
+const sp = sharepoint(process.env.SHAREPOINT_SITE_URL).auth(credentials);
+
+// retrieve all items from the list
+sp.list(process.env.SHAREPOINT_LIST_NAME).get((data) => {
+  console.log(data);
+}).catch((error) => {
+	console.log(error);
+});
